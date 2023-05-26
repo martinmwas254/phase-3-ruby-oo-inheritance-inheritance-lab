2a3b6d4e0f0dc1269121e50d9152d95fc48633bc
Martin <Martin@Martin>
2023-05-26 23:20:42 +0300

    completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,22 @@
-class Student 
+
+class Student < User
+
+    attr_accessor :knowledge
+
+    def initialize(knowledge = [] )
+
+        @knowledge = knowledge
+    end
+
+    def learn(new_knowledge)
+
+        @knowledge << new_knowledge
+
+    end
+
+    def show_knowledge
+        @knowledge
+    end
 
 end
+
